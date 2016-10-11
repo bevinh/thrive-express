d292e1ce2beeaddc6d25c4a9692c9e12a92ef85a
bevinh <admin_bah34@CMFR-143.local>
2016-10-11 19:32:54 -0400

    Handlebarred the course list

--- routes/course_list.js
+++ routes/course_list.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var courses = {
+    "Session 2": {
+        "title": "Session 2: Interesting Relevant Title Here",
+        "description": "Fanny pack vinyl put a bird in it",
+        "image": "https://dummyimage.com/150x150/000/fff",
+        "time" : "3 hours"
+    },
+    "Session 3": {
+        "title": "Session 3: This Title is pretty cool too",
+        "description": "Fanny pack vinyl put a bird in it",
+        "image": "https://dummyimage.com/150x150/000/fff",
+        "time" : "3 hours"
+    },
+    "Session 4": {
+        "title": "Session 4: Interesting Stuff here too",
+        "description": "Fanny pack vinyl put a bird in it",
+        "image": "https://dummyimage.com/150x150/000/fff",
+        "time" : "3 hours"
+    }
+}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('course_list', { title: 'Grow!'});
+  res.render('course_list', { title: 'Grow!', courses: courses});
 });
 
 module.exports = router;
