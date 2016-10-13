804997278aa9776ab6b37121a9dc714811bce0d7
bevinh <admin_bah34@client-104-39-60-48.mobility.psu.edu>
2016-10-13 11:07:06 -0400

    Added some buffer to the bottom of the course_list

--- routes/course_list.js
+++ routes/course_list.js
@@ -3,24 +3,24 @@ var router = express.Router();
 
 var courses = {
     "Session 2": {
-        "title": "Session 2: Interesting Relevant Title Here",
+        "title": "Session 1: Interesting Relevant Title Here",
         "description": "Fanny pack vinyl put a bird in it",
         "image": "https://dummyimage.com/150x150/000/fff",
-        "time" : "3 hours",
+        "time" : "20 minutes",
         "flag" : "active"
     },
     "Session 3": {
-        "title": "Session 3: This Title is pretty cool too",
+        "title": "Session 2: This Title is pretty cool too",
         "description": "Fanny pack vinyl put a bird in it",
         "image": "https://dummyimage.com/150x150/000/fff",
-        "time" : "3 hours",
+        "time" : "20 minutes",
         "flag" : "inactive"
     },
     "Session 4": {
-        "title": "Session 4: Interesting Stuff here too",
+        "title": "Session 3: Interesting Stuff here too",
         "description": "Fanny pack vinyl put a bird in it",
         "image": "https://dummyimage.com/150x150/000/fff",
-        "time" : "3 hours",
+        "time" : "20 minutes",
         "flag" : "inactive"
     }
 }
